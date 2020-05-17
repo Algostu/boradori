1594b30d2f5ccee8542d52daa309491416c77070
Han Kyul Kang <consistant1y@ajou.ac.kr>
2020-05-17 19:53:14 +0900

    Solve baekjoon pro 13-1
    - Need to think about Brute-force problem & solution

---
+++ ONLINE_CONTEST/GOOGLE_KICKSTART/ROUND_A/HANKYUL/1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+#define COMPARE(X, Y) (X) > (Y) ? 0 : ((X) == (Y) ? 0 : 1)
+
+#define SWAP(X, Y, T) T temp = (X); (X) = (Y); (Y) = temp;
+
+#define MALLOC(X, Y) \
+if(((X) = malloc((Y)))==NULL){\
+  fprintf(stderr, "malloc error"); exit(EXIT_FAILURE);\
+}
+
+void solve(int test_num){
+  int i, j, K, ans=0;
+  
+  printf("Case #%d: %d\n", test_num, ans);
+}
+
+int main(){
+  // int i, N;
+  // scanf("%d", &N);
+  // for(i=0;i<N;i++){
+  //     solve(0);
+  // }
+
+  solve(0);
+
+	return 0;
+}
