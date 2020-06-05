250d2e74c74ecbf3b66b5d5c906566c0f02f6a42
rhcsky <mangusn1@gmail.com>
2020-06-05 14:36:28 +0900

    BOJ solution update

--- beakjoon/rhcsky/16-15.cpp
+++ beakjoon/rhcsky/16-15.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    /* Code */
+    int n, input, maxi;
+    int dp[100001];
+    vector<int> v;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&input);
+        v.push_back(input);
+    }
+
+    dp[0] = v[0];
+    maxi = dp[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        dp[i] = max(dp[i-1] + v[i], v[i]);
+        maxi = max(dp[i],maxi);
+    }
+    
+    cout << maxi;
+    
     return 0;
 }
