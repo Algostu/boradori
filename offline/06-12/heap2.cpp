a39c807b029ced7ab88e8e7d0030e91608360cad
rhcsky <mangusn1@gmail.com>
2020-06-13 16:10:12 +0900

    Solution update about heap

--- offline/06-12/heap2.cpp
+++ offline/06-12/heap2.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
-int main()
+int zmain()
 {
     vector<int> v = {40,10,30,2,4,50};
     
@@ -18,5 +19,20 @@ int main()
     pop_heap(v.begin(),v.end());
     v.pop_back();
 
+    return 0;
+}
+
+int main(){
+    priority_queue<int, vector<int>, greater<int>> heap; //Min Heap
+    vector<int> a = {7,9,1,5,3,4,8};
+
+    for (auto &&i : a) heap.push(i);
+
+    for (int i = 0; i < 7; i++)
+    {
+        cout << heap.top();
+        heap.pop();
+    }
+    
     return 0;
 }
