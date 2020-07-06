8b3a60eae1fbda2225160f95e773dc6051cf46a4
rhcsky <mangusn1@gmail.com>
2020-07-06 20:50:53 +0900

    Rename file

--- ONLINE_CONTEST/UCPC/week1/rhc/A.cpp
+++ ONLINE_CONTEST/UCPC/week1/rhc/A.cpp
@@ -1,148 +1,198 @@
-#include <iostream>
-#include <limits>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-void FlipArray(std::vector<int>& array, int startIndex, int endIndex)
-{
-	int min = std::numeric_limits<int>::max();
-	int max = std::numeric_limits<int>::min();
+typedef long long ll;
 
-	int sum = 0;
+const int inf = 1e9+7;
 
-	for (int i = startIndex; i <= endIndex; i++)
-	{
-		if (min > array[i]) min = array[i];
-		if (max < array[i]) max = array[i];
+int arr[1010101];
 
-		sum += array[i];
-	}
+struct Node{
+    Node *l, *r, *p;
+    ll v, sz, sum, mx, mn;
+    bool dummy, flip;
 
-	std::reverse(array.begin() + startIndex, array.begin() + endIndex + 1);
-
-    std::cout << min << " " << max << " " << sum << '\n';
-}
-
-void ShiftArray(std::vector<int>& v, int startIndex, int endIndex, int shiftOffset)
-{
-    int min = std::numeric_limits<int>::max();
-	int max = std::numeric_limits<int>::min();
-
-	int sum = 0;
-
-	for (int i = startIndex - 1; i < endIndex; i++)
-	{
-		if (min > v[i]) min = v[i];
-		if (max < v[i]) max = v[i];
-
-		sum += v[i];
-	}
-
-    vector<int> vv;
-    for (size_t i = startIndex-1; i < endIndex; i++)
-    {
-        vv.push_back(v[i]);
-    } 
-	if (shiftOffset > 0){
-        shiftOffset = shiftOffset % vv.size();
-        rotate(vv.rbegin(),vv.rbegin()+shiftOffset,vv.rend());
-        copy(vv.begin(),vv.end(),v.begin()+startIndex-1);
+    Node(ll _v, Node *p) : v(_v), p(p) {
+        l = r = nullptr;
+        sum = mx = mn = _v;
+        sz = 1, dummy = 0, flip = 0;
     }
-    else 
-	{      
-        shiftOffset = (-shiftOffset) % vv.size();
-        rotate(vv.begin(),vv.begin()+shiftOffset,vv.end());
-        copy(vv.begin(),vv.end(),v.begin()+startIndex-1);
-	}
-	std::cout << min << " " << max << " " << sum << '\n';
-}
+    Node(int _v) : Node(_v, nullptr) {}
+    Node() : Node(0) {}
+    ~Node(){
+        if(l) delete l;
+        if(r) delete r;
+    }
+};
 
-void PrintArray(std::vector<int> array)
-{
-	for (int i = 0; i < array.size(); i++)
-	{
-		std::cout << array[i] << " ";
-	}
-	std::cout << std::endl;
-}
+struct SplayTree{
+    Node *root;
+    Node *ptr[1010101]; //node pointer
 
-int main()
-{
-	int n = 0;
-	std::cin >> n;
+    void update(Node *x){
+        x->sz = 1;
+        x->sum = x->mn = x->mx = x->v;
+        if(x->l){
+            x->sz += x->l->sz;
+            x->sum += x->l->sum;
+            x->mn = min(x->mn, x->l->mn);
+            x->mx = max(x->mx, x->l->mx);
+        }
+        if(x->r){
+            x->sz += x->r->sz;
+            x->sum += x->r->sum;
+            x->mn = min(x->mn, x->r->mn);
+            x->mx = max(x->mx, x->r->mx);
+        }
+    }
 
-	std::vector<int> array(n);
-	array.clear();
+    void push(Node *x){
+        if(!x->flip) return;
+        swap(x->l, x->r);
+        if(x->l) x->l->flip = !x->l->flip;
+        if(x->r) x->r->flip = !x->r->flip;
+        x->flip = 0;
+    }
 
-	for (int i = 0; i < n; i++) 
-	{
-		array.push_back(i + 1);
-	}
+    Node* gather(int s, int e){ //gather [s, e]
+        kth(e+1);
+        auto tmp = root;
+        kth(s-1);
+        splay(tmp, root);
+        return root->r->l;
+    }
 
-	int q = 0;
-	std::cin >> q;
+    void rotate(Node *x){
+        auto p = x->p;
+        Node *y;
+        push(p); push(x);
+        if(x == p->l){
+            p->l = y = x->r;
+            x->r = p;
+        }else{
+            p->r = y = x->l;
+            x->l = p;
+        }
+        x->p = p->p; p->p = x;
+        if(y) y->p = p;
+        if(x->p){
+            if(p == x->p->l) x->p->l = x;
+            else x->p->r = x;
+        }
+        else root = x;
+        update(p); update(x);
+    }
 
-	while (q > 0)
-	{
-		int question = 0;
-		std::cin >> question;
+    void splay(Node *x, Node *g = nullptr){
+        Node *y;
+        while(x->p != g){
+            Node *p = x->p;
+            if(p->p == g){
+                rotate(x); break;
+            }
+            auto pp = p->p;
+            if((p->l == x) == (pp->l == p)){
+                rotate(p); rotate(x);
+            }else{
+                rotate(x); rotate(x);
+            }
+        }
+        if(!g) root = x;
+    }
 
-		switch (question)
-		{
-			case 1:
-				{
-					int startIndex = 0;
-					int endIndex = 0;
+    SplayTree() : root() {
+        memset(ptr, 0, sizeof ptr);
+    }
+    ~SplayTree(){
+        if(root) delete root;
+    }
 
-					std::cin >> startIndex;
-					std::cin >> endIndex;
-					FlipArray(array, startIndex - 1, endIndex - 1); 
-				}
-				break;
-			case 2:
-				{
-					int startIndex = 0;
-					int endIndex = 0;
-					int shiftOffset = 0;
+    void init(int n){
+        if(root) delete root;
+        root = new Node(-inf); //left dummy node
+        auto now = root;
+        for(int i=1; i<=n; i++){
+            ptr[i] =  now->r = new Node(i, now);
+            now = now->r;
+        }
+        now->r = new Node(inf, now); //right dummy node
+        root->dummy = now->r->dummy = 1;
+        for(int i=n; i>=1; i--) update(ptr[i]);
+        splay(ptr[n/2]);
+    }
 
-					std::cin >> startIndex;
-					std::cin >> endIndex;
-					std::cin >> shiftOffset;
+    void flip(int s, int e){
+        Node *x = gather(s, e);
+        x->flip = !x->flip;
+    }
 
-					ShiftArray(array, startIndex, endIndex, shiftOffset);
-				}
-				break;
-			case 3:
-				{
-					int n;
-					std::cin >> n;
-					std::cout << array[n - 1] << std::endl;
-				}
-				break;
-			case 4:
-				{
-					int n = 0;
-					std::cin >> n;
-					for (int i = 0; i < array.size(); i++)
-					{
-						if (array[i] == n)
-						{
-							std::cout << i + 1 << std::endl;
-							break;
-						}
-					}
-				}
-				break;
-		}
+    void shift(int s, int e, int k){
+        Node *x = gather(s, e);
+        cout << x->mn << " " << x->mx << " " << x->sum << "\n";
+        if(k >= 0){
+            k %= (e - s + 1);
+            if(!k) return;
+            flip(s, e); flip(s, s+k-1); flip(s+k, e);
+        }else{
+            k *= -1;
+            k %= (e - s + 1);
+            if(!k) return;
+            flip(s, e); flip(s, e-k); flip(e-k+1, e);
+        }
+    }
 
-		q--;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		std::cout << array[i] << " ";
-	}
-	std::cout << std::endl;
+    void getidx(int k){
+        splay(ptr[k]);
+        cout << root->l->sz << "\n";
+    }
 
-	return 0;
+    void kth(int k){ //1-based
+        auto now = root;
+        push(now);
+        while(1){
+            while(now->l && now->l->sz > k){
+                now = now->l; push(now);
+            }
+            if(now->l) k -= now->l->sz;
+            if(!k) break; k--;
+            now = now->r;
+            push(now);
+        }
+        splay(now);
+    }
+
+    void print(Node *x){
+        push(x);
+        if(x->l) print(x->l);
+        if(!x->dummy) cout << x->v << " ";
+        if(x->r) print(x->r);
+    }
+}tree;
+
+int n, m, q;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> q;
+    tree.init(n);
+    while(q--){
+        int op; cin >> op;
+        if(op == 1){
+            int s, e; cin >> s >> e;
+            tree.flip(s, e);
+            Node *x = tree.gather(s, e);
+            cout << x->mn << " " << x->mx << " " << x->sum << "\n";
+        }else if(op == 2){
+            int s, e, k; cin >> s >> e >> k;
+            tree.shift(s, e, k);
+        }else if(op == 3){
+            int k; cin >> k;
+            tree.kth(k);
+            cout << tree.root->v << "\n";
+        }else if(op == 4){
+            int k; cin >> k;
+            tree.getidx(k);
+        }
+    }
+    tree.print(tree.root);
 }
