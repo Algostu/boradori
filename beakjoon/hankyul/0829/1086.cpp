168a38b9428466b965600529752b45e9493801e1
Han Kyul Kang <consistant1y@ajou.ac.kr>
2020-08-31 19:57:54 +0900

    make .clang-format

--- beakjoon/hankyul/0829/1086.cpp
+++ beakjoon/hankyul/0829/1086.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
-#include <string>
-#include <map>
-#include <vector>
-#include <queue>
-#include <list>
-#include <utility>
 #include <algorithm>
-#include <cstring>
 #include <cmath>
+#include <cstring>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
 
 #define FIXED_VAL 100000000
 #define FIXED_LEN 9
@@ -17,70 +17,89 @@ using namespace std;
 string numbers[16];
 int ans = 0;
 int total = 0;
+int fixedRem;
+string nums[15];
+int K, N;
 
-int recursive_modular(string a, int K, int fixedRem){
-  if(a.length() <= FIXED_LEN){
-    return stoi(a) % K;
-  } else {
-    int len = a.length();
-    return (((recursive_modular(a.substr(0, len - FIXED_LEN), K, fixedRem) % K) * fixedRem) % K + stoi(a.substr(len-FIXED_LEN, FIXED_LEN)) % K) % K;
-  }
-}
-
-string make_complete_str(int arr[], int n){
-  string result;
-
-  for (size_t i = 0; i < n; i++) {
-    result += numbers[arr[i]];
-  }
-  //cout << result << endl;
-  return result;
-}
-
-void perm(int arr[], int depth, int n, int K) {
-  if (depth == n) {
-    if (!recursive_modular(make_complete_str(arr, n), K, FIXED_VAL % K)){
-      ans++;
+int recursive_modular(string a)
+{
+    if (a.length() <= FIXED_LEN)
+    {
+        return stoi(a) % K;
     }
-    total++;
-    return;
-  }
-
-  for (int i = depth; i < n; i++) {
-    swap(arr[i], arr[depth]);
-    perm(arr, depth + 1, n, K);
-    swap(arr[i], arr[depth]);
-  }
+    else
+    {
+        int len = a.length();
+        int upperBitRem = recursive_modular(a.substr(0, len - FIXED_LEN)) % K;
+        int curBitRem = stoi(a.substr(len - FIXED_LEN, FIXED_LEN)) % K;
+        return ((upperBitRem * fixedRem) % K + curBitRem) % K;
+    }
 }
 
-int gcd(int a, int b){
-  int r = a % b;
-  if (r == 0) return b;
-  else return gcd(b, r);
+void recurse(int mask, int taken, string pref)
+{
+    if (taken == N)
+    {
+        return;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        if (((mask >> i) & 1) == 0)
+        {
+            recurse((mask | (1 << i)), taken + 1, pref + nums[i]);
+        }
+    }
 }
 
-void solve(int test_num = 0){
-  int N, K;
-  int arr [15];
-  cin >> N;
-  for (size_t i = 0; i < N; i++) {
-    cin >> numbers[i];
-    arr[i] = i;
-  }
-  cin >> K;
-  perm(arr, 0, N, K);
-
-  if(ans==0){
-    cout << "0/1\n";
-  } else if (ans == 1){
-    cout << "1/1\n";
-  } else{
-    int div = gcd(total, ans);
-    ans /= div; total /= div;
-    cout << ans << "/" << total << "\n";
-  }
+int gcd(int a, int b)
+{
+    int r = a % b;
+    if (r == 0)
+        return b;
+    else
+        return gcd(b, r);
 }
 
-int main(void){
-  solve();
+void solve(int test_num = 0)
+{
+    cin >> N;
+    vector<int> v(N);
+    for (size_t i = 0; i < N; i++)
+    {
+        cin >> nums[i];
+        v[i] = i;
+    }
+    cin >> K;
+    fixedRem = FIXED_VAL % K;
+    do
+    {
+        string result = "";
+        for (int i = 0; i < N; i++)
+        {
+            result += nums[v[i]];
+        }
+        total++;
+        if (recursive_modular(result) == 0)
+        {
+            ans++;
+        }
+    } while (next_permutation(v.begin(), v.end()));
+
+    if (ans == 0)
+    {
+        cout << "0/1\n";
+    }
+    else
+    {
+        int div = gcd(total, ans);
+        ans /= div;
+        total /= div;
+        cout << ans << "/" << total << "\n";
+    }
+}
+
+int main(void)
+{
+    freopen("../input.txt", "r", stdin);
+    solve();
 }
