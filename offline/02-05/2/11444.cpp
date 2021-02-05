e64b8ec0fffcdec6e0924c86d1be203926d33285
hankyul kang <consistant1y@ajou.ac.kr>
2021-02-05 17:02:31 +0900

    ADD GDB Option in Makefile

--- offline/02-05/2/11444.cpp
+++ offline/02-05/2/11444.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
+class sampleClass{
+   public: 
+        
+        int i = 0;
+        int p = 1;
+};
+
 void solve(){ 
     cout << "hello world\n"; 
     cout << "hello world this is master branch\n"; 
+    sampleClass inst = sampleClass();
+    // some logic go here
+    for (int i = 0; i<10; i++){
+        inst.i += 1;
+        inst.p += 10;
+        printf("%d\n", i);
+    }
     return;
 }
 
